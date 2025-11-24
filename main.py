bb526bbdcaaa128f2727500bb2c05f448cbe7dc4
bushbuckwhale-21 <bushbuckwhale@gmail.com>
2025-11-24 13:51:36 +0300

    changes

--- main.py
+++ main.py
@@ -4,7 +4,22 @@ from stats import get_char_frequencies
 def main():
     # path = input("Enter the full path to the book text file:")
     path = "./books/frankenstein.txt"
-    print(get_char_frequencies(path))
+    num_words = get_num_words(path)
+    frequencies = get_char_frequencies(path)
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    
+    sorted_frequencies = sorted(frequencies.items(), key=lambda x: x[1], reverse=True)
+
+    for key, item in sorted_frequencies:
+        if key.isalpha():
+            print(f"{key}: {item}")
+
+    print("============= END ===============")
 
 if __name__ == "__main__":
     main()
