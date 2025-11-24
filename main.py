ad2f9a0038f7b68b6e5225b34a563261fc8b8572
bushbuckwhale-21 <bushbuckwhale@gmail.com>
2025-11-24 13:21:28 +0300

    more progress

--- main.py
+++ main.py
@@ -2,13 +2,14 @@
 def get_book_text(filePath):
     with open(filePath) as file:
         file_contents = file.read()
-        return file_contents
+        words = file_contents.split()
+        return len(words)
 
 
 def main():
     # path = input("Enter the full path to the book text file:")
     path = "./books/frankenstein.txt"
-    print(get_book_text(path))
+    print("Found", get_book_text(path), "total words")
 
 if __name__ == "__main__":
     main()
